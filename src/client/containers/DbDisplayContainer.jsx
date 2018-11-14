476cf3e1a4b6ebe31d0bb2b14aa65a8f1a0a42eb
g-es <gesun@usc.edu>
2018-11-14 10:19:21 -0800

    added Refs to DbDisplay

--- src/client/containers/DbDisplayContainer.jsx
+++ src/client/containers/DbDisplayContainer.jsx
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import DbDisplay from '../components/DbDisplay.jsx';
 
-const DbDisplayContainer = (props) => {
-  const { db } = props;
-  const tables = db.map(tableInfo => <DbDisplay key={tableInfo.name} tableInfo={tableInfo} />);
+class DbDisplayContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allRefs: [],
+    };
+  }
 
-  return (
-    <div id="dbDisplayContainer">
-      {tables}
-    </div>
-  );
-};
+
+  componentDidMount() {
+    const { db } = this.props;
+    const arrTest = this.state.allRefs.slice();
+    db.forEach(ele => {
+      console.log(ele,'ele')
+      // ele.props.tableInfo.columns.forEach(e=>arrTest.push(e))
+    });
+    console.log(db, 'pray');
+  }
+
+  render() {
+    const { db } = this.props;
+    const tables = db.map(tableInfo => <DbDisplay key={tableInfo.name} tableInfo={tableInfo} ref={tableInfo.name} />);
+    // const arrTest = this.state.allRefs.slice();
+    // db.forEach(ele => {
+    //   console.log(arrTest,'TT',ele)
+    //   ele.columns.forEach(e=>arrTest.push(e))
+    // });
+    // console.log(tables, 'tables2');
+    return (
+      <div id="dbDisplayContainer">
+        {tables}
+      </div>
+    );
+  }
+
+  // return (
+  //   <div id="dbDisplayContainer">
+  //     {tables}
+  //   </div>
+  // );
+}
 
 export default DbDisplayContainer;
